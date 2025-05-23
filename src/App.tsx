32137d9c46b871e2c7a023d508832c2f4a29a608
Denariadna <149600221+Denariadna@users.noreply.github.com>
2025-05-24 01:46:00 +0300

    start build

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './main.css';
+
+const App = () => {
+    return (
+        <div>
+            <p className="text-5xl">работает</p>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import App from './App';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { StrictMode } from 'react';
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+    <StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </StrictMode>
+);
